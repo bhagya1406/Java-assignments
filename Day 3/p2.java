96bbbd7024d009052b06ebbd8591bbb607d9fe61
Bhagyashri <bsshintre77@gmail.com>
2024-10-02 21:50:22 +0530

    day 3 code added

---
+++ Day 3/p2.java
@@ -0,0 +1,13 @@
+class Demo{
+	public static void main(String[]args){
+		for(int i=1;i<=12;i++){
+			if(i%3==0){
+				System.out.print(i);
+			}else{
+				System.out.print(" ");
+			}
+		}
+	}
+}
+
+O/p:- 3  6  9  12
---
+++ Day 3/p5.java
@@ -0,0 +1,9 @@
+class Demo{
+	public static void main(String[]args){
+		for(char ch='a';ch<='d';ch++){
+			System.out.print(ch+" ");
+		}
+	}
+}
+
+O/p:- a b c d
