d8efbae83bc55a5563d98b089fa2ef5364238fb7
Bhagyashri <bsshintre77@gmail.com>
2024-10-03 21:34:14 +0530

    day 4 code added

---
+++ Day 4/p1.java
@@ -0,0 +1,10 @@
+class Demo{
+	public static void main(String[]args){
+		for(char ch='A';ch<='I';ch++){
+			System.out.print(ch+" ");
+			ch++;
+		}
+	}
+}
+
+O/p:- A C E G I
---
+++ Day 4/p5.java
@@ -0,0 +1,18 @@
+class Demo{
+	public static void main(String[]args){
+		for(int i=1;i<=3;i++){
+			for(int j=1;j<=4;j++){
+				if(j%2==1){
+					System.out.print("1 ");
+				}else{
+					System.out.print("0 ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
+
+O/p:-   1 0 1 0
+	1 0 1 0
+	1 0 1 0
