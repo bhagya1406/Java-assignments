38f647412928ffafce881ead0f095f77b6ff6d44
Bhagyashri <bsshintre77@gmail.com>
2024-10-08 19:14:53 +0530

    day 5 code added

---
+++ Day 5/p1.java
@@ -0,0 +1,18 @@
+class Demo{
+	public static void main(String[]args){
+	int a=20;
+	int b=30;
+	int c=40;
+		if(a>=b && a>=c){
+			System.out.println(a + " is the largest number");
+		}else if(b>=a && b>=c){
+			
+			System.out.println(b + " is the largest number");
+		}else{
+			
+			System.out.println(c + " is the largest number");
+		}
+	}
+}
+
+O/p:-	40 is the largest number
---
+++ Day 5/p3.java
@@ -0,0 +1,20 @@
+class Demo{
+	public static void main(String[]args){
+	char ch1='a';
+	char ch2='b';
+	int a=97;
+	int b=117;
+		if(a>=97 && b>=117){
+			System.out.println(ch1 + " is vowel");
+			System.out.println(ch2 + " is consonant");
+			a++;
+			b++;
+		}else{
+			System.out.println();
+		}
+	}
+}
+
+O/p:-	a is vowel
+	b is consonant
+
